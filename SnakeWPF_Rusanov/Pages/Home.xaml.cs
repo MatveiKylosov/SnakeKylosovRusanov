bab9dc5e807d1209a2e2d8c94e9ebf227136c223
Rusanov <Nikolay.Rusan0v@yandex.ru>
2024-11-22 02:34:40 +0500

    Исравление. Русанов

--- SnakeWPF_Rusanov/Pages/Home.xaml.cs
+++ SnakeWPF_Rusanov/Pages/Home.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SnakeWPF_Rusanov
+namespace SnakeWPF_Rusanov.Pages
 {
     /// <summary>
     /// Логика взаимодействия для Home.xaml
